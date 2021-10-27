fb17697ce825f52de7ddf942041ece710e68bc05
Tatsiana <t_a_b@tut.by>
2021-10-27 15:01:59 +0300

    feat: use pipe for read file task 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,19 +4,25 @@ function readFile() {
   const path = require('path');
   const fs = require('fs');
   const textPath = path.join(__dirname, 'text.txt');
-
-  const readableStream = fs.createReadStream(`${textPath}`, 'utf-8');
+  
+  const readableStream = fs.createReadStream(textPath, 'utf-8');
   //при работе с большим объемом данных чтобы не загружать оперативную память и не остановить работу программы на все время операции, считывание и запись данных можно осуществлять по частям (chunk).
   //вторым параметром передаем 'utf-8' чтобы преобразовать buffer в строку
   //readable stream используется для чтения данных
   //У потока чтения есть событие data, которое генерируется, когда стрим прочитал порцию данных и готов отдать ее потребителю этих данных.
   //т.к данные приходят частями, чтобы части собрать вместе используем переменную data
-  let data = '';
-  readableStream.on('data', chunk => data += chunk);
-  
+
+  // let data = '';
+  // readableStream.on('data', chunk => data += chunk);
+
   //чтобы знать, когда поток завершиться, используем событие стрима 'end', которое срабатывает когда все данные переданы
   
-  readableStream.on('end', () => console.log(data));
+  // readableStream.on('end', () => console.log(data));
+
+  //---или чтобы не создавать лишнюю переменную data используем объединение потоков. прочитанная информация сразу выводится 
+
+  readableStream.pipe(process.stdout);
+
   
   //обработаем ошибку
   
